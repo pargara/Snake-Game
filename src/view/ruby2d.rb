2925c7179e8b0720a2c39e280125b70e540159f5
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2022-12-19 20:17:00 -0500

    Vista basica sin terminar

---
+++ src/view/ruby2d.rb
@@ -0,0 +1,20 @@
+require "ruby2d"
+require_relative "../model/state"
+module View
+  class Ruby2dView
+
+    def initialize
+      @pixel_size = 50
+    end
+    
+    def render(state)
+      extend Ruby2D::DSL
+      set(
+        title:"Snake", 
+        width: pixel_size * state.grid.cols, 
+        height: pixel_size * state.grid.rows)
+      show
+    end
+
+  end
+end
