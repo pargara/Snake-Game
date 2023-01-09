005d012a7c9496fa99448b6b236fb3a172c79279
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-01-09 14:32:00 -0500

    27 y pequeños errores corregidos (puede que estos no tengan trazabilidad en Git)

--- src/view/ruby2d.rb
+++ src/view/ruby2d.rb
@@ -11,10 +11,37 @@ module View
       extend Ruby2D::DSL
       set(
         title:"Snake", 
-        width: pixel_size * state.grid.cols, 
-        height: pixel_size * state.grid.rows)
+        width: @pixel_size * state.grid.cols, 
+        height: @pixel_size * state.grid.rows)
+      render_food(state)
+      render_snake(state)
       show
     end
 
+    private
+
+    def render_food(state)
+      extend Ruby2D::DSL
+      food = state.food
+      Square.new(
+        x: food.col * @pixel_size,
+        y: food.row * @pixel_size,
+        size: @pixel_size,
+        color: "yellow",
+      )
+    end
+
+    def render_snake(state)
+      extend Ruby2D::DSL
+      snake = state.snake
+      snake.positions.each do |position|
+        Square.new(
+          x: position.col * @pixel_size,
+          y: position.row * @pixel_size,
+          size: @pixel_size,
+          color: "green",
+          )
+      end
+    end
   end
 end
