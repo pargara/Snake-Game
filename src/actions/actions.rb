ed10fc77165e903cdcca816dbb10f417a113a01e
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-01-14 12:36:33 -0500

    Test y solucion de algunos errores

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -2,7 +2,8 @@ module Actions
   def self.move_snake(state)
     next_direction = state.next_direction
     next_position = calc_next_position(state)
-    if position_is_valid?(state,next_position)
+    # verificar que la siguiente cassilla sea valida
+    if position_is_valid?(state, next_position)
       move_snake_to(state, next_position)
     else
       end_game(state)
@@ -10,37 +11,52 @@ module Actions
   end
 
   private
-
-  def calc_next_position(state)
-  curr_position = state.snake.positions.first
+  
+  def self.calc_next_position(state)
+    curr_position = state.snake.positions.first
     case state.next_direction
-      when UP
-        new_position = Model::Coord.new(curr_position.row -1, curr_position.col)
-      when RIGHT
-        new_position = Model::Coord.new(curr_position.row, curr_position.col + 1)
-      when DOWN
-        new_position = Model::Coord.new(curr_position.row +1, curr_position.col)
-      when LEFT
-        new_position = Model::Coord.new(curr_position.row, curr_position.col - 1)
-
-      end
+    when Model::Direction::UP
+      # decrementar fila
+      return Model::Coord.new(
+          curr_position.row - 1, 
+          curr_position.col)
+    when Model::Direction::RIGHT
+      # incrementar col
+      return Model::Coord.new(
+          curr_position.row, 
+          curr_position.col + 1)
+    when Model::Direction::DOWN
+      # incrementar fila
+      return Model::Coord.new(
+          curr_position.row + 1,
+          curr_position.col)
+    when Model::Direction::LEFT
+      # decrementar col
+      return Model::Coord.new(
+          curr_position.row, 
+          curr_position.col - 1)
+    end
   end
 
-  def position_is_valid?(state, next_position)
-    is_invalid ((position.row >= state.grid.rows || position.row < 0) || (position.col >= state.grid.col || position.col < 0))
-
+  def self.position_is_valid?(state, position)
+    # verificar q este en la grilla
+    is_invalid = ((position.row >= state.grid.rows ||
+      position.row < 0) || 
+      (position.col >= state.grid.cols ||
+      position.col < 0))
     return false if is_invalid
-   return !(state.snake.positions.include? position)
+    # verificar q no este superponiendo a la serpiente
+    return !(state.snake.positions.include? position)
   end
 
-    def move_snake_to(state, next_position)
-      new_positions = [next_position] + state.snake.positions[0...-1]
-      state.snake.positions = new_positions
-      state
-    end
-
-    def end_game(state)
-      state.game_finished = true
-      state
-    end
+  def self.move_snake_to(state, next_position)
+    new_positions = [next_position] + state.snake.positions[0...-1]
+    state.snake.positions = new_positions
+    state
   end
+
+  def self.end_game(state)
+    state.game_finished = true
+    state
+  end
+end
